24a9f1227d4d0b32bd9dcf355dcc8f94e498a4f6
Frank <frankiek3@gmail.com>
2015-02-05 20:40:20 -0500

    Update

--- app/models/product_filters_decorator.rb
+++ app/models/product_filters_decorator.rb
@@ -50,20 +50,12 @@ module Spree
         }
       end
 
-      # Example: filtering by price
-      #   The named scope just maps incoming labels onto their conditions, and builds the conjunction
-      #   'price' is in the base scope's context (ie, "select foo from products where ...") so
-      #     we can access the field right away
-      #   The filter identifies which scope to use, then sets the conditions for each price range
+      # Filter by price range
       Spree::Product.add_search_scope :price_range do |*opts|
         return all if opts.all?(&:blank?) || opts.length < 2
         opts[0] = opts[0].blank? || opts[0].to_f < 0 ? "0" : opts[0]
         price_between(*opts)
       end
-
-      #def ProductFilters.format_price(amount)
-      #  Spree::Money.new(amount)
-      #end
 
       def ProductFilters.price_range_filter
         {
@@ -73,78 +65,6 @@ module Spree
         }
       end
 
-
-      # Example: filtering by possible brands
-      #
-      # First, we define the scope. Two interesting points here: (a) we run our conditions
-      #   in the scope where the info for the 'brand' property has been loaded; and (b)
-      #   because we may want to filter by other properties too, we give this part of the
-      #   query a unique name (which must be used in the associated conditions too).
-      #
-      # Secondly, the filter. Instead of a static list of values, we pull out all existing
-      #   brands from the db, and then build conditions which test for string equality on
-      #   the (uniquely named) field "p_brand.value". There's also a test for brand info
-      #   being blank: note that this relies on with_property doing a left outer join
-      #   rather than an inner join.
-      #Spree::Product.add_search_scope :brand_any do |*opts|
-      #  conds = opts.map {|o| ProductFilters.brand_filter[:conds][o]}.reject { |c| c.nil? }
-      #  scope = conds.shift
-      #  conds.each do |new_scope|
-      #    scope = scope.or(new_scope)
-      #  end
-      #  Spree::Product.with_property('brand').where(scope)
-      #end
-
-      #def ProductFilters.brand_filter
-      #  brand_property = Spree::Property.find_by(name: 'brand')
-      #  brands = brand_property ? Spree::ProductProperty.where(property_id: brand_property.id).pluck(:value).uniq.map(&:to_s) : []
-      #  pp = Spree::ProductProperty.arel_table
-      #  conds = Hash[*brands.map { |b| [b, pp[:value].eq(b)] }.flatten]
-      #  {
-      #    name:   'Brands',
-      #    scope:  :brand_any,
-      #    conds:  conds,
-      #    labels: (brands.sort).map { |k| [k, k] }
-      #  }
-      #end
-
-      # Example: a parameterized filter
-      #   The filter above may show brands which aren't applicable to the current taxon,
-      #   so this one only shows the brands that are relevant to a particular taxon and
-      #   its descendants.
-      #
-      #   We don't have to give a new scope since the conditions here are a subset of the
-      #   more general filter, so decoding will still work - as long as the filters on a
-      #   page all have unique names (ie, you can't use the two brand filters together
-      #   if they use the same scope). To be safe, the code uses a copy of the scope.
-      #
-      #   HOWEVER: what happens if we want a more precise scope?  we can't pass
-      #   parametrized scope names to Ransack, only atomic names, so couldn't ask
-      #   for taxon T's customized filter to be used. BUT: we can arrange for the form
-      #   to pass back a hash instead of an array, where the key acts as the (taxon)
-      #   parameter and value is its label array, and then get a modified named scope
-      #   to get its conditions from a particular filter.
-      #
-      #   The brand-finding code can be simplified if a few more named scopes were added to
-      #   the product properties model.
-      #Spree::Product.add_search_scope :selective_brand_any do |*opts|
-      #  Spree::Product.brand_any(*opts)
-      #end
-
-      #def ProductFilters.selective_brand_filter(taxon = nil)
-      #  taxon ||= Spree::Taxonomy.first.root
-      #  brand_property = Spree::Property.find_by(name: 'brand')
-      #  scope = Spree::ProductProperty.where(property: brand_property).
-      #    joins(product: :taxons).
-      #    where("#{Spree::Taxon.table_name}.id" => [taxon] + taxon.descendants)
-      #  brands = scope.pluck(:value).uniq
-      #  {
-      #    name:   'Applicable Brands',
-      #    scope:  :selective_brand_any,
-      #    labels: brands.sort.map { |k| [k, k] }
-      #  }
-      #end
-      #Spree::Taxon.find(params[:taxon]) @properties[:taxon]
 
       Spree::Property.all.each do |p|
         method_any = p.name.downcase + "_any"
@@ -186,43 +106,7 @@ module Spree
         end
       end
 
-      # Provide filtering on the immediate children of a taxon
-      #
-      # This doesn't fit the pattern of the examples above, so there's a few changes.
-      # Firstly, it uses an existing scope which was not built for filtering - and so
-      # has no need of a conditions mapping, and secondly, it has a mapping of name
-      # to the argument type expected by the other scope.
-      #
-      # This technique is useful for filtering on objects (by passing ids) or with a
-      # scope that can be used directly (eg. testing only ever on a single property).
-      #
-      # This scope selects products in any of the active taxons or their children.
-      #
-      def ProductFilters.taxons_below(taxon)
-        return Spree::Core::ProductFilters.all_taxons if taxon.nil?
-        {
-          name:   'Taxons under ' + taxon.name,
-          scope:  :taxons_id_in_tree_any,
-          labels: taxon.children.sort_by(&:position).map { |t| [t.name, t.id] },
-          conds:  nil
-        }
-      end
 
-      # Filtering by the list of all taxons
-      #
-      # Similar idea as above, but we don't want the descendants' products, hence
-      # it uses one of the auto-generated scopes from Ransack.
-      #
-      # idea: expand the format to allow nesting of labels?
-      def ProductFilters.all_taxons
-        taxons = Spree::Taxonomy.all.map { |t| [t.root] + t.root.descendants }.flatten
-        {
-          name:   'All taxons',
-          scope:  :taxons_id_equals_any,
-          labels: taxons.sort_by(&:name).map { |t| [t.name, t.id] },
-          conds:  nil # not needed
-        }
-      end
     end
   end
 end
